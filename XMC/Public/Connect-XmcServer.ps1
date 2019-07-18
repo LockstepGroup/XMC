4281f707bebabc843ccb6191b8b5647b8571ac27
Brian Addicks <brian@addicks.us>
2019-07-18 10:56:47 -0400

    added Connect-XmcServer

---
+++ XMC/Public/Connect-XmcServer.ps1
@@ -0,0 +1,32 @@
+function Connect-XmcServer {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $True, Position = 0)]
+        [ValidatePattern("\d+\.\d+\.\d+\.\d+|(\w\.)+\w")]
+        [string]$Hostname,
+
+        [Parameter(Mandatory = $False, Position = 1)]
+        [int]$Port = 8443,
+
+        [Parameter(Mandatory = $True, Position = 2)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential
+    )
+
+    BEGIN {
+        $XmcServer = New-XmcServer
+        $XmcServer.Hostname = $Hostname
+        $XmcServer.Port = $Port
+        $XmcServer.Credential = $Credential
+
+    }
+
+    PROCESS {
+        $TestConnection = $XmcServer.invokeTestQuery()
+    }
+
+    END {
+        return $TestConnection
+    }
+}
